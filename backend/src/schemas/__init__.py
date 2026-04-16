c239a30bb55a91248f5561792fa0d602fe0264d4
VietLinh <nvlinh0607@gmail.com>
2026-04-16 14:51:17 +0700

    feat: add cv screening schemas

--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,21 +1,29 @@
 """Schemas."""
 
-from src.schemas.auth import (
-    GoogleTokenRequest,
-    GoogleUserInfo,
-    RefreshTokenRequest,
-    TokenResponse,
-    UserWithTokens,
+from src.schemas.auth import GoogleTokenRequest as GoogleAuthRequest
+from src.schemas.auth import TokenResponse
+from src.schemas.cv import (
+    CandidateProfilePayload,
+    CVScreeningPayload,
+    CVScreeningResponse,
+    MinimumRequirementCheck,
+    RequirementStatus,
+    ScreeningRecommendation,
 )
-from src.schemas.user import UserCreate, UserResponse, UserUpdate
+from src.schemas.jd import JDAnalysisPayload, JDAnalysisResponse, JDRecentItem
+from src.schemas.user import UserResponse
 
 __all__ = [
-    "GoogleTokenRequest",
-    "GoogleUserInfo",
-    "RefreshTokenRequest",
+    "CVScreeningPayload",
+    "CVScreeningResponse",
+    "CandidateProfilePayload",
+    "GoogleAuthRequest",
+    "JDAnalysisPayload",
+    "JDAnalysisResponse",
+    "JDRecentItem",
+    "MinimumRequirementCheck",
+    "RequirementStatus",
+    "ScreeningRecommendation",
     "TokenResponse",
-    "UserWithTokens",
-    "UserCreate",
     "UserResponse",
-    "UserUpdate",
 ]
